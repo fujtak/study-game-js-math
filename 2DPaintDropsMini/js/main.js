ad91c783d082f7ce37ba43b6e31217f7f394d670
fujtak <fuj.eng23@gmail.com>
2025-10-26 20:25:36 +0900

    描画ループを実装

--- 2DPaintDropsMini/js/main.js
+++ 2DPaintDropsMini/js/main.js
@@ -2,20 +2,20 @@
 
 let ctx
 
-function paint() {
-  ctx.clearRect(0, 0, 800, 500);
-  drawLine({x0: 100, y0: 100, x1: 200, y1: 200, width: 1, color: 'red'})
-}
-
-function mainloop() {
-  paint()
-  requestAnimationFrame(mainloop)
-}
-
 function init() {
   const canvas = document.querySelector('canvas')
   ctx = canvas.getContext('2d')
-  mainloop()
+  loop()
+}
+
+function loop() {
+  paint()
+  requestAnimationFrame(loop)
+}
+
+function paint() {
+  ctx.clearRect(0, 0, 800, 500);
+  drawLine({x0: 100, y0: 100, x1: 200, y1: 200, width: 1, color: 'red'})
 }
 
 window.addEventListener('load', init)
