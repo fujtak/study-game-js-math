5ae0b7fdc003cdc8c1f54003544e00705789b476
fujtak <fuj.eng23@gmail.com>
2025-12-06 18:34:41 +0900

    update: Listに追加できるようにしてみる

--- 2DPaintDropsMini/js/EntityLineList.js
+++ 2DPaintDropsMini/js/EntityLineList.js
@@ -2,15 +2,18 @@ import { Position } from "./Position.js"
 import { EntityLine } from "./EntityLine.js"
 
 class EntityLineList {
+  #lines
   constructor() {
-    this.lines = [
+    this.#lines = [
       new EntityLine(new Position(100, 100), new Position(300, 300)),
       new EntityLine(new Position(200, 100), new Position(400, 300)),
     ]
-    Object.freeze(this)
+  }
+  push(line) {
+    this.#lines.push(line)
   }
   draw() {
-    for(const line of this.lines) {
+    for(const line of this.#lines) {
       line.draw()
     }
   }
--- 2DPaintDropsMini/js/main.js
+++ 2DPaintDropsMini/js/main.js
@@ -1,9 +1,15 @@
+import { Position } from "./Position.js"
+import { EntityLine } from "./EntityLine.js"
 import { EntityLineList } from './EntityLineList.js'
 
 const ctx = document.querySelector('canvas').getContext('2d')
 Object.defineProperty(window, "CONTEXT", { value: ctx })
 
 const lines = new EntityLineList()
+setTimeout(() => {
+  const line = new EntityLine(new Position(300, 100), new Position(500, 300))
+  lines.push(line)
+}, 3000)
 
 function loop() {
   draw()
