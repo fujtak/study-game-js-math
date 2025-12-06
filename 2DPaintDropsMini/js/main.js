222b62433b8aaf2015cfcc7cc31a174dcc39e3b8
fujtak <fuj.eng23@gmail.com>
2025-12-06 18:19:20 +0900

    add: 線を1つ描画してみる

--- 2DPaintDropsMini/js/main.js
+++ 2DPaintDropsMini/js/main.js
@@ -1,3 +1,6 @@
+import { Position } from "./Position.js"
+import { EntityLine } from "./EntityLine.js"
+
 const ctx = document.querySelector('canvas').getContext('2d')
 Object.defineProperty(window, "CONTEXT", { value: ctx })
 
@@ -8,6 +11,10 @@ function loop() {
 
 function draw() {
   CONTEXT.clearRect(0, 0, CONTEXT.canvas.width, CONTEXT.canvas.height)
+  const start = new Position(100, 100)
+  const end = new Position(300, 300)
+  const line = new EntityLine(start, end)
+  line.draw()
 }
 
 window.addEventListener('load', loop)
