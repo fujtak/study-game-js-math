25ea0bba980f71b7fbda7c5a5042b31a1b720a37
fujtak <fuj.eng23@gmail.com>
2025-12-06 18:13:29 +0900

    delete: 作り直しのため削除

--- 2DPaintDropsMini/js/main.js
+++ 2DPaintDropsMini/js/main.js
@@ -1,9 +1,5 @@
-import { LineStrokedList } from "./LineStrokedList.js"
-
 const ctx = document.querySelector('canvas').getContext('2d')
 Object.defineProperty(window, "CONTEXT", { value: ctx })
-
-const lineStrokedList = new LineStrokedList()
 
 function loop() {
   draw()
@@ -12,7 +8,6 @@ function loop() {
 
 function draw() {
   CONTEXT.clearRect(0, 0, CONTEXT.canvas.width, CONTEXT.canvas.height)
-  lineStrokedList.draw()
 }
 
 window.addEventListener('load', loop)
