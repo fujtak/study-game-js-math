080c36b191733a105a319bc8a40e64995f7715ed
fujtak <fuj.eng23@gmail.com>
2025-12-06 18:25:41 +0900

    update: Listとして保持してみる

--- 2DPaintDropsMini/js/main.js
+++ 2DPaintDropsMini/js/main.js
@@ -4,6 +4,11 @@ import { EntityLine } from "./EntityLine.js"
 const ctx = document.querySelector('canvas').getContext('2d')
 Object.defineProperty(window, "CONTEXT", { value: ctx })
 
+const lines = [
+  new EntityLine(new Position(100, 100), new Position(300, 300)),
+  new EntityLine(new Position(200, 100), new Position(400, 300)),
+]
+
 function loop() {
   draw()
   requestAnimationFrame(loop)
@@ -11,10 +16,9 @@ function loop() {
 
 function draw() {
   CONTEXT.clearRect(0, 0, CONTEXT.canvas.width, CONTEXT.canvas.height)
-  const line = new EntityLine(new Position(100, 100), new Position(300, 300))
-  line.draw()
-  const line2 = new EntityLine(new Position(200, 100), new Position(400, 300))
-  line2.draw()
+  for(const line of lines) {
+    line.draw()
+  }
 }
 
 window.addEventListener('load', loop)
