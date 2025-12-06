a5b54dc9d55894fd5b1a20fc75552f81a6b58f8d
fujtak <fuj.eng23@gmail.com>
2025-12-06 19:22:26 +0900

    refact: Listを整理

--- 2DPaintDropsMini/js/EntityLineList.js
+++ 2DPaintDropsMini/js/EntityLineList.js
@@ -1,22 +1,13 @@
-import { Position } from "./Position.js"
-import { EntityLine } from "./EntityLine.js"
-
 class EntityLineList {
-  #lines
   constructor() {
-    this.#lines = [
-      new EntityLine(new Position(100, 100), new Position(300, 300)),
-      new EntityLine(new Position(200, 100), new Position(400, 300)),
-    ]
-  }
-  get latest() {
-    return this.#lines.at(-1)
+    this.lines = []
+    Object.freeze(this)
   }
   push(line) {
-    this.#lines.push(line)
+    this.lines.push(line)
   }
   draw() {
-    for(const line of this.#lines) {
+    for(const line of this.lines) {
       line.draw()
     }
   }
