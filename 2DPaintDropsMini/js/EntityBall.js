f66dc676a870c245528a698249d05bcc7ea43ae5
fujtak <fuj.eng23@gmail.com>
2026-01-10 16:55:13 +0900

    add: 画面外判定

--- 2DPaintDropsMini/js/EntityBall.js
+++ 2DPaintDropsMini/js/EntityBall.js
@@ -3,15 +3,38 @@ import { Position } from "./Position.js"
 class EntityBall {
   #element
   #speed
+  #size
   #position
   constructor(x) {
     this.#element = document.querySelector('[data-ball]')
     this.#speed = 1
+    this.#size = 40
     this.#position = new Position(x, 0)
+  }
+  get #left() {
+    return this.#position.x
+  }
+  get #right() {
+    return this.#position.x + this.#size
+  }
+  get #top() {
+    return this.#position.y
+  }
+  get #bottom() {
+    return this.#position.y + this.#size
+  }
+  get #isOnscreenX() {
+    return 0 <= this.#right && this.#left <= CONTEXT.canvas.width
+  }
+  get #isOnscreenY() {
+    return 0 <= this.#bottom && this.#top <= CONTEXT.canvas.height
+  }
+  get isOnscreen() {
+    return this.#isOnscreenX && this.#isOnscreenY
   }
   draw() {
     this.#position = new Position(this.#position.x, this.#position.y + this.#speed)
-    CONTEXT.drawImage(this.#element, this.#position.x, this.#position.y, 40, 40)
+    CONTEXT.drawImage(this.#element, this.#position.x, this.#position.y, this.#size, this.#size)
   }
 }
 
