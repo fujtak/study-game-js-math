dc7d498a59f05bf2156436136a6f8187b116bd16
fujtak <fuj.eng23@gmail.com>
2026-01-10 16:40:04 +0900

    add: ボールを落下させてみる

--- 2DPaintDropsMini/js/EntityBall.js
+++ 2DPaintDropsMini/js/EntityBall.js
@@ -2,12 +2,15 @@ import { Position } from "./Position.js"
 
 class EntityBall {
   #element
+  #speed
   #position
   constructor(x) {
     this.#element = document.querySelector('[data-ball]')
+    this.#speed = 1
     this.#position = new Position(x, 0)
   }
   draw() {
+    this.#position = new Position(this.#position.x, this.#position.y + this.#speed)
     CONTEXT.drawImage(this.#element, this.#position.x, this.#position.y, 40, 40)
   }
 }
