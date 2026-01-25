3f6615e7ce2e95818686a1913bbfc38c2ba0167a
fujtak <fuj.eng23@gmail.com>
2026-01-25 19:45:41 +0900

    add: 重力

--- 2DPaintDropsMini/js/EntityBall.js
+++ 2DPaintDropsMini/js/EntityBall.js
@@ -7,6 +7,8 @@ class EntityBall {
     this.size = 40
     this.point = (y != undefined) ? new Point(x, y) : new Point(x, -this.size)
     this.speed = 1
+    this.maxSpeed = 1
+    this.gravity = 0.005
     this.velocity = velocity ? velocity : new Vector(0, this.speed)
     Object.freeze(this)
   }
@@ -24,10 +26,12 @@ class EntityBall {
     return isOnscreenX && isOnscreenY
   }
   get next() {
-    const x = this.point.x + this.velocity.x
-    const y = this.point.y + this.velocity.y
-    const point = new Point(x, y)
-    return new EntityBall(point.x, point.y, this.velocity)
+    const isAccelingY = (this.velocity.y < this.maxSpeed)
+    const accelY = isAccelingY ? this.gravity : 0
+    const velocity = new Vector(this.velocity.x, this.velocity.y + accelY)
+    const x = this.point.x + velocity.x
+    const y = this.point.y + velocity.y
+    return new EntityBall(x, y, velocity)
   }
   draw() {
     CONTEXT.drawImage(this.element, this.point.x, this.point.y, this.size, this.size)
