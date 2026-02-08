e9bf380bcf5d7e832063e60e9b1515612d8a213b
fujtak <fuj.eng23@gmail.com>
2026-02-08 20:31:01 +0900

    add: 頂点の3次元ベクトルの配列を取得

--- VRMLViewer/js/main.js
+++ VRMLViewer/js/main.js
@@ -1,3 +1,5 @@
+import { Vector3D } from './Vector3D.js'
+
 function getArrayFromString(keyword) {
   const positionPoint = text.indexOf(keyword)
   const positionPointStart = text.indexOf('[', positionPoint)
@@ -13,3 +15,7 @@ function getArrayFromString(keyword) {
 const response = await fetch('/VRMLViewer/wrl/cube.wrl')
 const text = await response.text()
 const points = getArrayFromString('points')
+const vertices = points
+  .filter((point, index) => (index % 3 !== 0))
+  .map((point, index) => new Vector3D(points[index], points[index + 1], points[index + 2]))
+console.log('vertices:', vertices)
