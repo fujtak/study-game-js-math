905ff200252c1e39e4f08350aec8576167bcefaf
fujtak <fuj.eng23@gmail.com>
2026-02-08 19:57:18 +0900

    refact: 関数化

--- VRMLViewer/js/main.js
+++ VRMLViewer/js/main.js
@@ -1,8 +1,13 @@
 const response = await fetch('/VRMLViewer/wrl/cube.wrl')
 const text = await response.text()
-const positionPoint = text.indexOf('point')
-const positionPointStart = text.indexOf('[', positionPoint)
-const positionPointEnd = text.indexOf(']', positionPoint)
-const textPoint = text.substring(positionPointStart + 1, positionPointEnd)
-const points = textPoint.trim().split(/[\s,]+/)
-console.log('points', points)
+
+function getArrayFromString(keyword) {
+  const positionPoint = text.indexOf(keyword)
+  const positionPointStart = text.indexOf('[', positionPoint)
+  const positionPointEnd = text.indexOf(']', positionPoint)
+  const textPoint = text.substring(positionPointStart + 1, positionPointEnd)
+  const points = textPoint.trim().split(/[\s,]+/)
+  return points
+}
+
+console.log('points', getArrayFromString('points'))
