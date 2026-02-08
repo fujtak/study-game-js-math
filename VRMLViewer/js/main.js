682ddbeef9027e8f057af37b60d465b51a57cf21
fujtak <fuj.eng23@gmail.com>
2026-02-08 20:35:34 +0900

    refact: 適切な命名

--- VRMLViewer/js/main.js
+++ VRMLViewer/js/main.js
@@ -1,19 +1,19 @@
 import { Vector3D } from './Vector3D.js'
 
-function getArrayFromString(keyword) {
-  const positionPoint = text.indexOf(keyword)
-  const positionPointStart = text.indexOf('[', positionPoint)
-  const positionPointEnd = text.indexOf(']', positionPoint)
-  const textPoint = text.substring(positionPointStart + 1, positionPointEnd)
-  const points = textPoint.trim().split(/[\s,]+/)
+function getIntegersFromString(keyword) {
+  const positionKeyword = text.indexOf(keyword)
+  const positionStart = text.indexOf('[', positionKeyword)
+  const positionEnd = text.indexOf(']', positionKeyword)
+  const textArray = text.substring(positionStart + 1, positionEnd)
+  const integers = textArray.trim().split(/[\s,]+/)
     .map(string => parseFloat(string))
     .filter(number => Number.isInteger(number))
-  return points
+  return integers
 }
 
 const response = await fetch('/VRMLViewer/wrl/cube.wrl')
 const text = await response.text()
-const points = getArrayFromString('points')
+const points = getIntegersFromString('points')
 const vertices = points
   .filter((point, index) => (index % 3 !== 0))
   .map((point, index) => new Vector3D(points[index], points[index + 1], points[index + 2]))
