647ed097e79aead7a2ab5bf0ba9024e0ae38e0b4
fujtak <fuj.eng23@gmail.com>
2026-02-19 23:30:50 +0900

    refact: 関数名を適切に

--- VRMLViewer/js/main.js
+++ VRMLViewer/js/main.js
@@ -1,6 +1,6 @@
 import { Vector3D } from './Vector3D.js'
 
-function getIntegersFromString(keyword) {
+function getIntegersFromWrl(keyword) {
   const positionKeyword = text.indexOf(keyword)
   const positionStart = text.indexOf('[', positionKeyword)
   const positionEnd = text.indexOf(']', positionKeyword)
@@ -13,7 +13,7 @@ function getIntegersFromString(keyword) {
 
 const response = await fetch('/VRMLViewer/wrl/cube.wrl')
 const text = await response.text()
-const points = getIntegersFromString('points')
+const points = getIntegersFromWrl('points')
 const vertices = points
   .filter((point, index) => (index % 3 !== 0))
   .map((point, index) => new Vector3D(points[index], points[index + 1], points[index + 2]))
