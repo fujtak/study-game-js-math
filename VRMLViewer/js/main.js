51aa47587a1f5dbf6b43d1dd405b883edec4d11e
fujtak <fuj.eng23@gmail.com>
2026-02-21 18:30:24 +0900

    refact: 適切に関数化

--- VRMLViewer/js/main.js
+++ VRMLViewer/js/main.js
@@ -1,20 +1,25 @@
 import { Vector3D } from './Vector3D.js'
 
-function getIntegersFromWrl() {
+function getPointsFlat(text) {
   const positionKeyword = text.indexOf('points')
   const positionStart = text.indexOf('[', positionKeyword)
   const positionEnd = text.indexOf(']', positionKeyword)
   const textArray = text.substring(positionStart + 1, positionEnd)
-  const integers = textArray.trim().split(/[\s,]+/)
+  const flat = textArray.trim().split(/[\s,]+/)
     .map(string => parseFloat(string))
     .filter(number => Number.isInteger(number))
-  return integers
+  return flat
 }
 
-const response = await fetch('/VRMLViewer/wrl/cube.wrl')
-const text = await response.text()
-const points = getIntegersFromWrl()
-const vertices = points
-  .filter((point, index) => (index % 3 !== 0))
-  .map((point, index) => new Vector3D(points[index], points[index + 1], points[index + 2]))
-console.log('vertices:', vertices)
+async function getPoints() {
+  const response = await fetch('/VRMLViewer/wrl/cube.wrl')
+  const text = await response.text()
+  const flat = getPointsFlat(text)
+  const points = flat
+    .filter((point, index) => (index % 3 !== 0))
+    .map((point, index) => new Vector3D(flat[index], flat[index + 1], flat[index + 2]))
+  return points
+}
+  
+const points = await getPoints()
+console.log('points:', points)
