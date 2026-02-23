01433d7004e612ebd3bb9576091831d049aedaa7
fujtak <fuj.eng23@gmail.com>
2026-02-23 15:59:40 +0900

    refact: メソッド名を適切に

--- VRMLViewer/js/ActionSelectModelTrigger.js
+++ VRMLViewer/js/ActionSelectModelTrigger.js
@@ -6,14 +6,14 @@ class ActionSelectModelTrigger {
     this.element = element
     Object.freeze(this)
   }
-  async #paint() {
+  async #onClick() {
     const id = this.element.dataset.triggerId
     const model = await Model.for(id)
     const viewer = new Viewer({ model })
     viewer.paint()
   }
   initialize() {
-    this.element.addEventListener('click', () => this.#paint())
+    this.element.addEventListener('click', () => this.#onClick())
   }
   emulate() {
     this.element.click()
