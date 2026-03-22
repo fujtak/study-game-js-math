8fdd4ca4ab7ef4333ad02e684dcc1a65c62b9c00
fujtak <fuj.eng23@gmail.com>
2026-03-22 23:34:46 +0900

    change: 回転速度を調整

--- VRMLViewer/js/Vector.js
+++ VRMLViewer/js/Vector.js
@@ -11,7 +11,7 @@ export class Vector {
     return Object.keys(this).length === 0
   }
   rotateY() {
-    const m = Matrix.forRotateY(0.2).list
+    const m = Matrix.forRotateY(0.1).list
     const x = this.x * m[0] + this.y * m[1] + this.z * m[2]
     const y = this.x * m[3] + this.y * m[4] + this.z * m[5]
     const z = this.x * m[6] + this.y * m[7] + this.z * m[8]
