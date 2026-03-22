951699e8f32226f91fc4c62b7bb316e1740a401a
fujtak <fuj.eng23@gmail.com>
2026-03-22 19:52:21 +0900

    wip: 一時的に3Dモデル変更機能を削除する（スムーズに機能追加するため）

--- VRMLViewer/js/main.js
+++ VRMLViewer/js/main.js
@@ -1,7 +1,21 @@
-import { ActionSelectModel } from './ActionSelectModel.js'
+import { VRML } from './VRML.js'
 
 const context = document.querySelector('canvas').getContext('2d')
 Object.defineProperty(window, 'CONTEXT', { value: context })
 
-const action = new ActionSelectModel()
-action.initialize()
+const vrml = await VRML.for('fruit-banana')
+const model = vrml.model
+
+function paint() {
+  context.fillStyle = 'black'
+  context.fillRect(0, 0, context.canvas.width, context.canvas.height)
+  context.strokeStyle = 'white'
+  model.paint()
+}
+
+function loop() {
+  paint()
+  requestAnimationFrame(loop)
+}
+
+loop()
